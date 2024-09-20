a41fc5b94c9a408f0b1abdc3845dcc6e0a176dd0
preetyrai11 <drpreetyrai@gmail.com>
2024-09-20 21:20:03 +0530

    added code

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,7 @@
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
-
+from langchain.text_splitter import CharacterTextSplitter
 
 #Extract data from the PDF
 def load_pdf(data):
@@ -17,14 +17,22 @@ def load_pdf(data):
 
 #Create text chunks
 def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
-    text_chunks = text_splitter.split_documents(extracted_data)
-
-    return text_chunks
+    # Splitting the data into chunk
+   text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=30, separator="\n")
+   text_chunks = text_splitter.split_documents(documents=extracted_data)   
+   return text_chunks 
 
 
 
 #download embedding model
 def download_hugging_face_embeddings():
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    #loading the embedding model from huggingface
+    embedding_model_name = "sentence-transformers/all-mpnet-base-v2"
+    # model_kwargs = {"device": "cuda"}
+    model_kwargs = {"device": "cpu"}
+    embeddings = HuggingFaceEmbeddings(
+       model_name=embedding_model_name,
+       model_kwargs=model_kwargs
+    )
     return embeddings
+
